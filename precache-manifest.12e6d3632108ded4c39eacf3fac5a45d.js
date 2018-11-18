083ae3473dd291d5850ae7ed3b1e2fecf3776ff7
miklik99 <40503150+miklik99@users.noreply.github.com>
2018-11-18 23:16:49 +0100

    Add files via upload

---
+++ precache-manifest.12e6d3632108ded4c39eacf3fac5a45d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "31920a33fed93cbb2d8248e1694e09bf",
+    "url": "/static/media/majales.31920a33.svg"
+  },
+  {
+    "revision": "e25f04a6b7e4d89c1a41f372ee830a94",
+    "url": "/static/media/asset4.e25f04a6.jpg"
+  },
+  {
+    "revision": "05f4f0d6e7f02345feef90b4b619dfd1",
+    "url": "/static/media/asset3.05f4f0d6.jpg"
+  },
+  {
+    "revision": "2b21634d6491d9b04d198d49e67ca80c",
+    "url": "/static/media/asset2.2b21634d.jpg"
+  },
+  {
+    "revision": "9813d6acd4372f0adc62599d81cd2e65",
+    "url": "/static/media/asset1.9813d6ac.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d675490928a824ce5c77",
+    "url": "/static/js/main.d6754909.chunk.js"
+  },
+  {
+    "revision": "5e14613f9e15f9f3b8af",
+    "url": "/static/js/1.5e14613f.chunk.js"
+  },
+  {
+    "revision": "d675490928a824ce5c77",
+    "url": "/static/css/main.532289f5.chunk.css"
+  },
+  {
+    "revision": "597403865549534757eaf26dfb1f399d",
+    "url": "/index.html"
+  }
+];
